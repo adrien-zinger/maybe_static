36ec1ab131dd974802a74dd8b92102dade1b3abb
Adrien Zinger <zinger.ad@gmail.com>
2023-04-20 11:11:31 +0200

    Trivial initialization + doc

--- src/lib.rs
+++ src/lib.rs
@@ -13,17 +13,7 @@
 
 #[macro_export]
 
-/// ```
-/// use anyhow::Result;
-/// pub fn get_ip(opt: Option<(&str, &str)>) -> Result<&'static String> {
-///    maybe_static!(opt, String, |(addr, port)| format!("{addr}:{port}"))
-/// }
-///
-/// fn main() {
-///     println!("{}", get_ip(Some(("hello", "world"))).unwrap());
-///     println!("{}", get_ip(None).unwrap());
-/// }
-/// ```
+/// Create an immutable (safe in any case) Meyer's singleton in the current scope.
 macro_rules! maybe_static {
     ($arg:expr, $t:ty, $fn:expr) => {{
         use std::{mem::MaybeUninit, sync::Once};
@@ -43,9 +33,36 @@ macro_rules! maybe_static {
             }
         }
     }};
+
+    ($t:ty, $fn:expr) => {{
+        use std::{mem::MaybeUninit, sync::Once};
+
+        static mut LAZY: (MaybeUninit<$t>, Once) = (MaybeUninit::uninit(), Once::new());
+        unsafe {
+            static mut ERR: bool = false;
+            LAZY.1.call_once(|| {
+                LAZY.0.write($fn());
+            });
+            &*LAZY.0.as_mut_ptr() // you're sure that it's initialized here
+        }
+    }};
+
+    ($t:ty) => {{
+        use std::{mem::MaybeUninit, sync::Once};
+
+        static mut LAZY: (MaybeUninit<$t>, Once) = (MaybeUninit::uninit(), Once::new());
+        unsafe {
+            static mut ERR: bool = false;
+            LAZY.1.call_once(|| {
+                LAZY.0.write(Default::default());
+            });
+            &*LAZY.0.as_mut_ptr() // you're sure that it's initialized here
+        }
+    }};
 }
 
 #[macro_export]
+/// Create an mutable (not safe in multithreading contexts) Meyer's singleton in the current scope.
 macro_rules! maybe_static_unsafe {
     ($arg:expr, $t:ty, $fn:expr) => {{
         use std::{mem::MaybeUninit, sync::Once};
@@ -74,6 +91,19 @@ macro_rules! maybe_static_unsafe {
             static mut ERR: bool = false;
             LAZY.1.call_once(|| {
                 LAZY.0.write($fn());
+            });
+            &mut *LAZY.0.as_mut_ptr() // you're sure that it's initialized here
+        }
+    }};
+
+    ($t:ty) => {{
+        use std::{mem::MaybeUninit, sync::Once};
+
+        static mut LAZY: (MaybeUninit<$t>, Once) = (MaybeUninit::uninit(), Once::new());
+        unsafe {
+            static mut ERR: bool = false;
+            LAZY.1.call_once(|| {
+                LAZY.0.write(Default::default());
             });
             &mut *LAZY.0.as_mut_ptr() // you're sure that it's initialized here
         }
